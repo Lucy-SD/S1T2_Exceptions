3bcab80dfb17906a80dfe279c04d7f0a4bec386a
~ Lucy ~ <castrob.lucia@gmail.com>
2025-06-29 20:36:22 +0200

    Delete S1T2_Exceptions/src/level2/excercise1 directory

--- S1T2_Exceptions/src/level2/excercise1/exceptions/InvalidStringException.java
+++
@@ -1,8 +0,0 @@
-package level2.excercise1.exceptions;
-
-public class InvalidStringException extends Exception {
-
-    public InvalidStringException(String message) {
-        super(message);
-    }
-}
